4096deb741a9a88970f8ac178cd27b3a7e68dc7e
Yauheni <truhanzhenya@mail.ru>
2019-07-20 10:54:09 +0300

    Занятие 18

---
+++ Занятие 18/src/main/java/model/User.java
@@ -0,0 +1,71 @@
+package model;
+
+public class User
+{
+    private int id;
+    private String login;
+    private String password;
+    private String name;
+    private String gender;
+    private String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
